7f47088e64f832ef4cfb942a781bad1a34989f0e
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-02-06 21:27:38 +0100

    added template and strings exercise folders

--- basics.js
+++ basics.js
@@ -1,2 +1,43 @@
+"use strict";
+
 // window.addEventListener("load", start);
 console.log("howdy");
+
+// y = 14
+
+const bool = true;
+const num = 41
+const str = "Peter";
+const obj = { cats: 2, cars: 1 };
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+console.log(n1 + n2);
+console.log(s1 + s2);
+
+console.log(n1 + s2);
+console.log(s1 + n2);
+
+
+
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log(7 / 0);
+console.log(" -9 " + 5);
+console.log(" -9 " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
